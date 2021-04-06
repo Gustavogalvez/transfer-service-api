3e2a6f35371f22b05d558104dfc7ccd656435469
Gustavo Galvez <gustavogalveza@gmail.com>
2021-04-05 21:57:03 -0400

    Revert "Heroku"
    This reverts commit 3742144ec1cce105afde836b29123b0df4616224.

--- api/addressee/addressee.controller.ts
+++
@@ -1,8 +0,0 @@
-import { Request, Response } from 'express';
-import { getListOfAddressee } from './addressee.service';
-
-export function getAddressees(req: Request, res: Response) {
-    getListOfAddressee().then(resp => {
-        res.send(resp.rows);
-    })
-}
--- api/addressee/index.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import {
-    getAddressees
-} from './addressee.controller';
-const router: Router = Router();
-
-router.get('/list', getAddressees);
-
-module.exports = router;
